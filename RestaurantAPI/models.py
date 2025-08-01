87107742a3d36878707cf1a4b7b32bbf329da561
BehnamNahari <behnam.nhri@gmail.com>
2025-08-01 06:54:54 -0700

    Restaurant-API
    define models

---
+++ RestaurantAPI/models.py
@@ -0,0 +1,81 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.name
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=25)
+    price = models.FloatField()
+    description = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)\
+
+    def __str__(self):
+        return self.name
+
+class Rating(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    score = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)])
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'menu_item')
+
+    def __str__(self):
+        return f"{self.user.username} - {self.menu_item.name}: {self.score}/10"
+
+
+class Order(models.Model):
+    STATUS_CHOICES = [
+        ('Pending', 'Pending'),
+        ('Paid', 'Paid'),
+        ('Cancelled', 'Cancelled'),
+        ('Preparing', 'Preparing'),
+        ("Delivered", "Delivered"),
+        ("Deliving", "Deliving"),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='Pending')
+    delivery_address = models.TextField()
+    total_price = models.FloatField()
+
+    def __str__(self):
+        return f"Order #{self.id} - {self.user.username} - {self.requested_at}"
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.quantity} x {self.menu_item.name}"
+
+
+class Reservation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    requested_at = models.DateTimeField()
+    note = models.TextField()
+    guests_count = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        f"reserved for {self.user.username} at {self.requested_at}"
+
+
+class ReservationItem(models.Model):
+    reservation = models.ForeignKey(Reservation, on_delete=models.CASCADE)
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.quantity} x {self.menu_item.name}"
